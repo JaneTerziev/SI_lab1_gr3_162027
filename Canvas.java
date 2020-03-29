10e739a3d2b2c4fd25997d9efc58dfc6db7120cf
Jane Terziev <janeterziev@gmail.com>
2020-03-29 03:42:20 +0200

    Added Canvas class to project

---
+++ Canvas.java
@@ -0,0 +1,15 @@
+class Canvas {
+    List<Point> pointList;
+
+    public Canvas(List<Point> pointList) {
+        this.pointList = pointList;
+    }
+
+    public List<Point> getPointList() {
+        return pointList;
+    }
+
+    public void setPointList(List<Point> pointList) {
+        this.pointList = pointList;
+    }
+}
