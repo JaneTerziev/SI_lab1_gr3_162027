aaa3235c574bd3db20bfb9b4bf4c426e0a5bdf57
Jane Terziev <janeterziev@gmail.com>
2020-03-29 03:39:38 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -7,16 +7,68 @@ class Point {
 
 	//TODO constructor
 
+	public Point(String id, double x, double y, String color) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
 	//TODO setters and getters
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection == 'L'){
+			this.x -= 1;
+		}
+		else  if (xDirection == 'R'){
+			this.x += 1;
+		}
+		if(yDirection =='U'){
+			this.y += 1;
+		}
+		else if (yDirection == 'D'){
+			this.y -= 1;
+		}
 	}
+
 
 	public void draw () {
 		//TODO
+		System.out.println(this.x);
+		System.out.println(this.y);
 	}
-
-
-
 }
